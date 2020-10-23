9428421e3345d31da3b0bd3dec41dbf0b2eee1f3
Nikos Roussos <nikos@roussos.cc>
2020-10-23 12:11:04 +0300

    Move script

--- tasks/post-deploy/04-activate-cloudflare.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-# Automatically update the DB if needed
-wp plugin activate cloudflare
-wp option update cloudflare_api_email "$CLOUDFLARE_EMAIL"
-wp option update cloudflare_api_key "$CLOUDFLARE_TOKEN"
