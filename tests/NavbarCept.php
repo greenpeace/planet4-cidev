066cb48def669263df5ebd2797657b4ba252d614
Nikos Roussos <nikos@roussos.cc>
2022-02-16 09:56:54 +0200

    Remove unused test

--- tests/NavbarCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-$I = new AcceptanceTester($scenario);
-$I->amGoingTo('Check navbar has right design');
-
-$I->amOnPage('/');
-
-$headerBackground = $I->getCSSValue('nav#header', 'background');
-
-$I->assertEquals('rgba(7, 67, 101, 0.8) none repeat scroll 0% 0% / auto padding-box border-box', $headerBackground);
