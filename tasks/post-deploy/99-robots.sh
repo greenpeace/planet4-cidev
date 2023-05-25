9d80971c423a80f6f0d500b53b52291e4b61b9e0
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-05-25 14:01:26 +0300

    Fix script path

--- tasks/post-deploy/99-robots.sh
+++ tasks/post-deploy/99-robots.sh
@@ -2,7 +2,7 @@
 
 if [ "$APP_ENV" = "production" ]; then
   echo "Adjusting robots file for production"
-  echo "User-agent: *" >source/public/robots.txt
-  echo "Disallow: /wp-admin/" >>source/public/robots.txt
-  echo "Sitemap: https://www.${APP_HOSTNAME}/${APP_HOSTPATH}/sitemap.xml" >>source/public/robots.txt
+  echo "User-agent: *" >public/robots.txt
+  echo "Disallow: /wp-admin/" >>public/robots.txt
+  echo "Sitemap: https://www.${APP_HOSTNAME}/${APP_HOSTPATH}/sitemap.xml" >>public/robots.txt
 fi
