c54ccf47971075f82a8847c523d4c049074b88c8
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-05-25 12:34:04 +0300

    Test robots with a script

---
+++ tasks/post-deploy/99-robots.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$APP_ENVIRONMENT" = 'production' ]; then
+  echo "Adjusting robots file for production"
+  echo "User-agent: *" >source/public/robots.txt
+  echo "Disallow: /wp-admin/" >>source/public/robots.txt
+  echo "Sitemap: https://www.${APP_HOSTNAME}/${APP_HOSTPATH}/sitemap.xml" >>source/public/robots.txt
+fi
