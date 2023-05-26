140ce87ca0186b5afa724865172c5e5981470d53
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-05-26 13:37:06 +0300

    Try running script in OpenResty pods

--- tasks/post-deploy/99-openresty-robots.sh
+++ tasks/post-deploy/99-openresty-robots.sh
@@ -4,5 +4,5 @@ if [ "$APP_ENV" = "production" ]; then
   echo "Adjusting robots file for production"
   echo "User-agent: *" >public/robots.txt
   echo "Disallow: /wp-admin/" >>public/robots.txt
-  echo "Sitemap: https://www.${APP_HOSTNAME}/${APP_HOSTPATH}/sitemap.xml" >>public/robots.txt
+  echo "Sitemap: https://www.${APP_HOSTNAME}/${APP_HOSTPATH}/sitemap.xml" >>source/public/robots.txt
 fi
