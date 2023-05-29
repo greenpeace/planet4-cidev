bc7c1b49714968958ecd9c14b491cc4bdc9396ad
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-05-29 18:01:54 +0300

    Test sentry builder

--- tasks/post-deploy/99-openresty-robots.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$APP_ENV" = "production" ]; then
-  echo "Adjusting robots file for production"
-  echo "User-agent: *" >public/robots.txt
-  echo "Disallow: /wp-admin/" >>public/robots.txt
-  echo "Sitemap: https://www.${APP_HOSTNAME}/${APP_HOSTPATH}/sitemap.xml" >>source/public/robots.txt
-fi
